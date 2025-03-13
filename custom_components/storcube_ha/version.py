0a320c46246b133cb5bbdc629cae4862aee44efa
jon7119 <168350835+jon7119@users.noreply.github.com>
2025-03-13 15:54:44 +0100

    Delete custom_components/storcube_ha directory

--- custom_components/storcube_ha/version.py
+++
@@ -1,7 +0,0 @@
-"""Version de l'intégration Storcube Battery Monitor."""
-
-# Format: x.y.z
-# x: Changements majeurs qui peuvent nécessiter des modifications de configuration
-# y: Nouvelles fonctionnalités
-# z: Corrections de bugs et améliorations mineures
-__version__ = "1.0.0"  # Version mise à jour 
