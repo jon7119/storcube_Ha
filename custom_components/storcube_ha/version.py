e2ad66f44cc0b76ae6dcab7b6a479bf17bc02a0b
jon7119 <168350835+jon7119@users.noreply.github.com>
2025-09-09 20:20:27 +0200

    Update version.py
    V1.3.0-beta1

--- custom_components/storcube_ha/version.py
+++ custom_components/storcube_ha/version.py
@@ -1,4 +1,5 @@
 """Version de l'intégration Storcube Battery Monitor."""
 
-__version__ = "B1.0.0"  # Changez ce numéro de version selon vos besoins 
+__version__ = "V1.3.0-beta1"  # Changez ce numéro de version selon vos besoins 
+
 
